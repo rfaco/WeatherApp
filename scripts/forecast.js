d15eb1598cd242d65ac09fd20cd5aa13e9c5531b
Roger Faco <49955609+rfaco@users.noreply.github.com>
2021-11-09 23:47:20 -0300

    Weather App Page

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = '682500PcukwQUtq1UDd6XimUfAmBA5HL'
+
+
+// GET weather information
+const getWeather = async (id) => {
+
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+}
+
+//GET City information
+const getCity = async (city) => {
+  
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
